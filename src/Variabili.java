ff1a0dde896b7f5d0bc86a08d17b36f1cbf2b8d3
Ruma303 <matteoh8@hotmail.it>
2023-08-10 13:52:51 +0200

    first commit

---
+++ src/Variabili.java
@@ -0,0 +1,18 @@
+public class Variabili {
+    public static void main(String[] args) {
+        int numero; // Dichiarazione
+        numero = 10; // Assegnazione
+        int numero2 = 20; // Inizializzazione
+
+        //System.out.println(numero);
+        //System.out.println(numero2);
+
+        System.out.println("numero: " + numero);
+        System.out.println("numero2: " + numero2);
+
+        int a = 10, b, c = 30;
+        System.out.println(b = 10);
+        System.out.println(b);
+        int d = 10; boolean e = false;
+    }
+}
